84698661b969149c2c21abde99246834019a7643
yogiex <priyagung.yoggi@gmail.com>
2020-11-04 00:03:32 +0700

    add event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 const buletan = document.getElementsByName('titik');
+const stop = document.getElementById('stop')
+const judul = document.getElementById('judul')
+var len = buletan.length;
+
+const off = function() {
+    judul.style.animation = "none";
+
+    for(var i=0;i< len;i++){
+        buletan[i].style.animation = "none";
+        buletan[i].style.background = "#563260";
+    }
+} 
+
+stop.addEventListener('click',off);
